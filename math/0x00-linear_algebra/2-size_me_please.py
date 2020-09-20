7711d4355dfde42b00c5e4b196aa4f197bc6134c
Luis Angel Vargas Mosquera <luisangelvargas91@gmail.com>
2020-09-20 11:28:32 -0500

    without nunpy

--- math/0x00-linear_algebra/2-size_me_please.py
+++ math/0x00-linear_algebra/2-size_me_please.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 """ Matrix Shape """
-import numpy as np
 
 
 def matrix_shape(matrix):
     """ Get the shape of a matrix """
-    arr = np.array(matrix)
-    return list(arr.shape)
+    arr = [len(matrix), len(matrix[0])]
+    if len(matrix[0]) > 2:
+        arr.append(len(matrix[0][0]))
+    return arr
