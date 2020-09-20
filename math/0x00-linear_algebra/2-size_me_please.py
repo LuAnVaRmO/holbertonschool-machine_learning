66c23a2979b6d16047ff949dcf9cb6a4641fa399
Luis Angel Vargas Mosquera <luisangelvargas91@gmail.com>
2020-09-20 11:15:04 -0500

    changing the file

---
+++ math/0x00-linear_algebra/2-size_me_please.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+""" Matrix Shape """
+import numpy as np
+
+
+def matrix_shape(matrix):
+    """ Get the shape of a matrix """
+    arr = np.array(matrix)
+    return arr.shape
